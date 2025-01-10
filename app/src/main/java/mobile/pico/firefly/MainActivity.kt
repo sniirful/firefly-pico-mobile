8419d8798e46ca13b1dbf1ae62ac333bf1cf87fa
Vincenzo <sniirful@gmail.com>
2025-01-10 06:22:48 +0100

    Added WebView in the MainActivity with all the needed functionalities.

--- app/src/main/java/mobile/pico/firefly/MainActivity.kt
+++ app/src/main/java/mobile/pico/firefly/MainActivity.kt
@@ -1,47 +1,84 @@
 package mobile.pico.firefly
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import mobile.pico.firefly.ui.theme.FireflyPicoMobileTheme
+import androidx.compose.ui.viewinterop.AndroidView
+import mobile.pico.firefly.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            FireflyPicoMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            AppTheme {
+                WebViewScreen("http://10.10.10.10:6976")
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @SuppressLint("SetJavaScriptEnabled")
+    @Composable
+    fun WebViewScreen(url: String) {
+        val canGoBack = remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FireflyPicoMobileTheme {
-        Greeting("Android")
+        DisposableEffect(Unit) {
+            onDispose {
+                webView.destroy()
+            }
+        }
+
+        BackHandler(enabled = canGoBack.value) {
+            webView.goBack()
+        }
+
+        AndroidView(factory = { context ->
+            WebView(context).apply {
+                overScrollMode = WebView.OVER_SCROLL_NEVER
+
+                webView = this
+                webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        canGoBack.value = view?.canGoBack() ?: false
+                    }
+                }
+                settings.apply {
+                    javaScriptEnabled = true
+                    domStorageEnabled = true
+                }
+                CookieManager.getInstance().setAcceptCookie(true)
+                fitsSystemWindows = true
+
+                loadUrl(url)
+            }
+        }, modifier = Modifier.fillMaxSize(), update = { view ->
+            webView = view
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        CookieManager.getInstance().flush()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webView.saveState(Bundle())
     }
 }
